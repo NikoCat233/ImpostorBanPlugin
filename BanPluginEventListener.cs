2a7184324d1bc2975a6f095a5a9266879f7adad0
Niko233 <139348239+NikoCat233@users.noreply.github.com>
2024-09-25 23:50:46 +0800

    Plugin Base

---
+++ BanPluginEventListener.cs
@@ -0,0 +1,27 @@
+﻿using Impostor.Api.Events;
+using Microsoft.Extensions.Logging;
+
+namespace ImpostorBanPlugin
+{
+    public class BanPluginEventListener :IEventListener
+    {
+        private readonly ILogger<BanPlugin> _logger;
+
+        public BanPluginEventListener(ILogger<BanPlugin> logger)
+        {
+            _logger = logger;
+        }
+
+        [EventListener]
+        public void OnPlayerJoined(IGamePlayerJoinedEvent e)
+        {
+            var player = e.Player;
+
+            if (player.Client.Puid == string.Empty)
+                return;
+
+            string puid = player.Client.Puid;
+
+        }
+    }
+}
--- ImpostorBanPlugin.cs
+++ ImpostorBanPlugin.cs
@@ -1,61 +1,33 @@
-using System.Threading.Tasks;
 using Impostor.Api.Events.Managers;
 using Impostor.Api.Plugins;
 using Microsoft.Extensions.Logging;
 
-namespace BanPlugin
+namespace ImpostorBanPlugin
 {
-    /// <summary>
-    ///     The metadata information of your plugin, this is required.
-    /// </summary>
-    [ImpostorPlugin(
-        package: "me.niko233.banplugin",
-        name: "BanPlugin",
-        author: "NikoCat233",
-        version: "1.0.0")]
-    public class BanPlugin : PluginBase // This is also required ": PluginBase".
+    [ImpostorPlugin("niko233.BanPlugin")]
+    public class BanPlugin : PluginBase
     {
-        /// <summary>
-        ///     A logger that works seamlessly with the server.
-        /// </summary>
         private readonly ILogger<BanPlugin> _logger;
+        private readonly IEventManager _eventManager;
+        private IDisposable _unregister;
 
-        /// <summary>
-        ///     The constructor of the plugin. There are a few parameters you can add here and they
-        ///     will be injected automatically by the server, two examples are used here.
-        ///
-        ///     They are not necessary but very recommended.
-        /// </summary>
-        /// <param name="logger">
-        ///     A logger to write messages in the console.
-        /// </param>
-        /// <param name="eventManager">
-        ///     An event manager to register event listeners.
-        ///     Useful if you want your plugin to interact with the game.
-        /// </param>
-        public BanPlugin(ILogger<ExamplePlugin> logger, IEventManager eventManager)
+        public BanPlugin(ILogger<BanPlugin> logger, IEventManager eventManager)
         {
             _logger = logger;
+            _eventManager = eventManager;
         }
 
-        /// <summary>
-        ///     This is called when your plugin is enabled by the server.
-        /// </summary>
-        /// <returns></returns>
         public override ValueTask EnableAsync()
         {
-            _logger.LogInformation("Ban plugin is being enabled.");
+            _logger.LogInformation("Ban plugin is enabled.");
+            _unregister = _eventManager.RegisterListener(new BanPluginEventListener(_logger));
             return default;
         }
 
-        /// <summary>
-        ///     This is called when your plugin is disabled by the server.
-        ///     Most likely because it is shutting down, this is the place to clean up any managed resources.
-        /// </summary>
-        /// <returns></returns>
         public override ValueTask DisableAsync()
         {
-            _logger.LogInformation("Ban plugin is being disabled.");
+            _logger.LogInformation("Ban plugin is disabled.");
+            _unregister.Dispose();
             return default;
         }
     }
