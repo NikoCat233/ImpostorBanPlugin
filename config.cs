3c61c5e8245a0c67e1085000e0c41273ca4b45b2
Niko233 <139348239+NikoCat233@users.noreply.github.com>
2024-09-26 10:55:49 +0800

    Update config

--- config.cs
+++ config.cs
@@ -5,6 +5,6 @@ namespace ImpostorBanPlugin
         public bool UseEac { get; set; } = false;
         public string EacBanMessage { get; set; } = "You are found on the TEN EAC cloud.\nYou are banned from this server.\n你因为在TEN EAC云上而被封禁。";
         public string CustomBanMessage { get; set; } = "You are on the server's ban list.\nYou are banned from this server.\n你已被此服务器封禁。";
-        public string BanListLocation { get; set; } = "./banlist.txt";
+        public string BanListLocation { get; set; } = "";
     }
 }
