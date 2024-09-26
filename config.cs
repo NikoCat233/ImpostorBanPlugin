b0f4f61ce7f963cb7dfbde555e72998d6640b0a5
Niko233 <139348239+NikoCat233@users.noreply.github.com>
2024-09-26 11:52:09 +0800

    Add url link to config

--- config.cs
+++ config.cs
@@ -3,8 +3,8 @@ namespace ImpostorBanPlugin
     public class Config
     {
         public bool UseEac { get; set; } = false;
-        public string EacBanMessage { get; set; } = "You are found on the TEN EAC cloud.\nYou are banned from this server.\n你因为在TEN EAC云上而被封禁。";
-        public string CustomBanMessage { get; set; } = "You are on the server's ban list.\nYou are banned from this server.\n你已被此服务器封禁。";
+        public string EacBanMessage { get; set; } = "You are found on the TEN EAC cloud.\nYou are banned from this server.\n你因为在TEN EAC云上而被封禁。\n<nobr><link=\"https://au.niko233.me\">Contact Server Admin | 联系我们</nobr></link>";
+        public string CustomBanMessage { get; set; } = "You are on the server's ban list.\nYou are banned from this server.\n你已被此服务器封禁。\n<nobr><link=\"https://au.niko233.me\">Contact Server Admin | 联系我们</nobr></link>";
         public string BanListLocation { get; set; } = "";
     }
 }
